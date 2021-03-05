71d4db00f3550c0a8f88a2b398bbf1ca8a7e8bf8
Anthony Rees <anthony_rees01@hotmail.com>
2021-03-05 15:17:29 +1100

    Update lambda_function_simple.py

--- lambda/lambda_function_simple.py
+++ lambda/lambda_function_simple.py
@@ -3,8 +3,10 @@ import urllib3
 import os
 import ssl
 import boto3
+import argparse
 
 codecommit = boto3.client('codecommit')
+region = codecommit.meta.region_name
 
 def lambda_handler(event, context):
     #Parse URL Parameters
@@ -16,12 +18,11 @@ def lambda_handler(event, context):
     port = str(port_a)
     token_a = os.environ["token"]
     token = str(token_a)
-    #print("*** Connected to %s as %s" % (http_method, url))
-    print("*** Event details" % (event))
 
     #Build Full URL out of Parameters
     full_url = http_method+'://'+url+':'+port+'/services/collector/raw'
     
+    #Do not check SSL certs
     cert_reqs = ssl.CERT_NONE
     urllib3.disable_warnings()
     http = urllib3.PoolManager(cert_reqs = cert_reqs)
@@ -29,25 +30,28 @@ def lambda_handler(event, context):
     
     #Log the updated references from the event
     references = { reference['ref'] for reference in event['Records'][0]['codecommit']['references'] }
-    print("***References: "  + str(references))
     
     #Get the repository from the event and show its git clone URL
     repository = event['Records'][0]['eventSourceARN'].split(':')[5]
+    region = event['Records'][0]['eventSourceARN'].split(":")[3]
+    branch = event['Records'][0]['codecommit']['references'][0]['ref'].split("/")[2];
+    source_version = event['Records'][0]['codecommit']['references'][0]['commit']
+    commit_time = event['Records'][0]['eventTime']
+    eventTriggerName = event['Records'][0]['eventTriggerName']
+    userIdentityARN =  event['Records'][0]['userIdentityARN'].split(':')[5]
+    ##print("*** Event is %s" % (event))
+    
     try:
         response = codecommit.get_repository(repositoryName=repository)
+        response1 = '{codecommit: data, repository_http : '+response['repositoryMetadata']['cloneUrlHttp']+', referncces : '+str(references)+', region : '+region+', branch : '+branch+', source_version : '+source_version+', commit_time : '+commit_time+', eventTriggerName : '+eventTriggerName+', userIdentityARN : '+userIdentityARN+'}'
         
-        print("Clone URL: " +response['repositoryMetadata']['cloneUrlHttp'])
-        #return response['repositoryMetadata']['cloneUrlHttp']
     except Exception as e:
         print(e)
         print('Error getting repository {}. Make sure it exists and that your repository is in the same region as this function.'.format(repository))
         raise e
-    
+
     #Post Event
-    r = http.request('POST', full_url, body=response['repositoryMetadata']['cloneUrlHttp'], headers={'Content-Type': 'application/json', 'Authorization':'Splunk '+token})
-    #r = http.request('POST', full_url, body='TestMe', headers={'Content-Type': 'application/json', 'Authorization':'Splunk '+token})
-    ##r = http.request('POST', full_url, body=event["body"], headers={'Content-Type': 'application/json', 'Authorization':'Splunk '+token})
-    print("*** R is %s" % (r))
+    r = http.request('POST', full_url, body=response1, headers={'Content-Type': 'application/json', 'Authorization':'Splunk '+token})
 
     return {
         'statusCode': 200,
